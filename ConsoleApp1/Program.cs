b0f4d2c1988b570ed4cb3690f0fe94a60b260f40
viso123 <viso91zhang@gmail.com>
2017-05-08 21:09:45 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthName mn = MonthName.March;
+            Console.WriteLine("{0}", (byte)mn);
+            return;
+        }
+    }
+    public enum MonthName : byte
+    {
+        January, Feburary, March
+    }
+}
